03818ba43a67f3aad544a8151499c58298c49e4a
Bernardo Gurgel <bernardogfilho@gmail.com>
2013-11-14 17:54:40 -0300

    Initial setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tutasks::Application.config.secret_key_base = '0d48d1468508b81f131a828c79d34992243c760b8e9602304fec1af605c1a828ec99561a6f09abca784522b9d5211b2bbe72d04890975e1bd7c0810c2ed4f37c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tutasks::Application.config.session_store :cookie_store, key: '_tutasks_session'
