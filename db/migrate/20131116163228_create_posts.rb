fc2e8d67ae85c9b9f32e7c8cd81ed3b5a4cf36ce
Bernardo Gurgel <bernardogfilho@gmail.com>
2013-11-16 13:56:32 -0300

    Apply STI in topic and question model with post model

---
+++ db/migrate/20131116163228_create_posts.rb
@@ -0,0 +1,13 @@
+class CreatePosts < ActiveRecord::Migration
+  def change
+    create_table :posts do |t|
+      t.string :type
+      t.string :title
+      t.text :content
+      t.references :project, index: true
+      t.references :task, index: true
+
+      t.timestamps
+    end
+  end
+end
