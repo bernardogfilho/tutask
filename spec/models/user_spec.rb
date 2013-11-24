1ed886d2c47025fb684f27a1ab36775e431b2135
Bernardo Gurgel <bernardogfilho@gmail.com>
2013-11-24 02:35:03 -0300

    Add topic/user relationship

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe User do
   it {should have_and_belong_to_many :projects}
+  it {should have_many :topics }
   it {should validate_presence_of(:name).on(:update)}
   it {should have_a_valid_factory}
 end
